3237a08069ac814665102982603c70d3a4ef4ce3
Ricardo Rivera <ricardorivera@Ricardos-MacBook-Air.local>
2019-06-05 12:11:26 -0500

    updated to ejs

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 var express = require("express");
 
 var app = express();
-
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/static"));
 app.get('/', function(request, response) {
-    app.use(express.static(__dirname + "/static"));
 
     response.send("<h1>Hello Express</h1>")
+})
+app.get("/cars", function(request, response) {
+    response.render('cars');
+})
+app.get('/cats', function(request, response){
+    response.render('cats')
+})
+app.get('/cars/new', function(request, response){
+    response.render('new')
 })
 
 app.listen(8000, function() {
